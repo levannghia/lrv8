1045b5d704f2b93905811bc305df0af237f18e95
levannghia <levannghia.tdc2018@gmail.com>
2021-07-15 15:50:11 +0700

    commit lai db

--- laravel.sql
+++ laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 14, 2021 at 03:40 AM
+-- Generation Time: Jul 15, 2021 at 08:48 AM
 -- Server version: 8.0.21
 -- PHP Version: 7.3.21
 
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `migrations`
@@ -63,7 +63,134 @@ CREATE TABLE IF NOT EXISTS `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1);
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2021_07_14_061504_create_posts_table', 2),
+(5, '2016_06_01_000001_create_oauth_auth_codes_table', 3),
+(6, '2016_06_01_000002_create_oauth_access_tokens_table', 3),
+(7, '2016_06_01_000003_create_oauth_refresh_tokens_table', 3),
+(8, '2016_06_01_000004_create_oauth_clients_table', 3),
+(9, '2016_06_01_000005_create_oauth_personal_access_clients_table', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_access_tokens`
+--
+
+DROP TABLE IF EXISTS `oauth_access_tokens`;
+CREATE TABLE IF NOT EXISTS `oauth_access_tokens` (
+  `id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint UNSIGNED DEFAULT NULL,
+  `client_id` bigint UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `scopes` text COLLATE utf8mb4_unicode_ci,
+  `revoked` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `expires_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `oauth_access_tokens_user_id_index` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `oauth_access_tokens`
+--
+
+INSERT INTO `oauth_access_tokens` (`id`, `user_id`, `client_id`, `name`, `scopes`, `revoked`, `created_at`, `updated_at`, `expires_at`) VALUES
+('7f0cf88f57a5217f94e5319e418d0595561aa577b7475146c250a3fce4376080bbbc5d54bf4caf48', 6, 1, 'lrv8', '[]', 0, '2021-07-14 20:29:35', '2021-07-14 20:29:35', '2022-07-15 03:29:35'),
+('a657acc1cd6919558c1543d7d1304bb84527255c1e629ed80d219420348e2348d07c0687afad46e3', 6, 1, 'lrv8', '[]', 0, '2021-07-14 20:33:08', '2021-07-14 20:33:08', '2022-07-15 03:33:08'),
+('4646bf0387764022fc6ba142e9f2e308bdf4fd7fbade9fcba17f515565483efc5e606ec6334e9b7b', 6, 1, 'lrv8', '[]', 0, '2021-07-14 20:41:10', '2021-07-14 20:41:10', '2022-07-15 03:41:10'),
+('a18c895025906851e0cbe7d7a7bb04879d1388bba12eecf11a8a7d3d48132a213203317485f5dbe9', 12, 1, 'lrv8', '[]', 0, '2021-07-14 21:08:58', '2021-07-14 21:08:58', '2022-07-15 04:08:58'),
+('4df1f84c224f6f9955d0cbf23850913f62c6d7939b3914191dc10ea8130746f53a5543b54793bc06', 12, 1, 'lrv8', '[]', 0, '2021-07-14 23:07:42', '2021-07-14 23:07:42', '2022-07-15 06:07:42'),
+('703803274cf20dfd428b78be20b5f598dfcc5402c3b416089def3f4522c04119f20b06afebe34142', 12, 1, 'lrv8', '[]', 0, '2021-07-15 01:23:54', '2021-07-15 01:23:54', '2022-07-15 08:23:54'),
+('40f424ef312fd40c37f014332fcb72e81d3c8e953ac2c36d6647f9d88764ddd7b4e2904565620b13', 12, 1, 'lrv8', '[]', 0, '2021-07-15 01:24:08', '2021-07-15 01:24:08', '2022-07-15 08:24:08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_auth_codes`
+--
+
+DROP TABLE IF EXISTS `oauth_auth_codes`;
+CREATE TABLE IF NOT EXISTS `oauth_auth_codes` (
+  `id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint UNSIGNED NOT NULL,
+  `client_id` bigint UNSIGNED NOT NULL,
+  `scopes` text COLLATE utf8mb4_unicode_ci,
+  `revoked` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `oauth_auth_codes_user_id_index` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_clients`
+--
+
+DROP TABLE IF EXISTS `oauth_clients`;
+CREATE TABLE IF NOT EXISTS `oauth_clients` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` bigint UNSIGNED DEFAULT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `secret` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `provider` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `redirect` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `personal_access_client` tinyint(1) NOT NULL,
+  `password_client` tinyint(1) NOT NULL,
+  `revoked` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `oauth_clients_user_id_index` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `oauth_clients`
+--
+
+INSERT INTO `oauth_clients` (`id`, `user_id`, `name`, `secret`, `provider`, `redirect`, `personal_access_client`, `password_client`, `revoked`, `created_at`, `updated_at`) VALUES
+(1, NULL, 'Laravel Personal Access Client', 'F9ijrhHDiXsgi8n4oL46DF5M3MCZINfvuja36sgO', NULL, 'http://localhost', 1, 0, 0, '2021-07-14 19:08:04', '2021-07-14 19:08:04'),
+(2, NULL, 'Laravel Password Grant Client', 'v22TlQTbbsVliHmE3PHRYmrfc38HxK2Q6Zo94Opp', 'users', 'http://localhost', 0, 1, 0, '2021-07-14 19:08:04', '2021-07-14 19:08:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_personal_access_clients`
+--
+
+DROP TABLE IF EXISTS `oauth_personal_access_clients`;
+CREATE TABLE IF NOT EXISTS `oauth_personal_access_clients` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `client_id` bigint UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `oauth_personal_access_clients`
+--
+
+INSERT INTO `oauth_personal_access_clients` (`id`, `client_id`, `created_at`, `updated_at`) VALUES
+(1, 1, '2021-07-14 19:08:04', '2021-07-14 19:08:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_refresh_tokens`
+--
+
+DROP TABLE IF EXISTS `oauth_refresh_tokens`;
+CREATE TABLE IF NOT EXISTS `oauth_refresh_tokens` (
+  `id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `access_token_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `revoked` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `oauth_refresh_tokens_access_token_id_index` (`access_token_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -78,6 +205,34 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
   `created_at` timestamp NULL DEFAULT NULL,
   KEY `password_resets_email_index` (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `permission` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `post` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `permission`, `post`, `created_at`, `updated_at`) VALUES
+(1, 'Nghia u3', 'Public', 'saafafsdf', NULL, '2021-07-15 01:44:15'),
+(2, 'Chieu mua buon 2', 'Private', 'asdfgdhfj5656v ddrer', NULL, '2021-07-14 01:06:46'),
+(5, 'Chieu mua buon 2', 'Public', 'ewewf', '2021-07-14 01:16:09', '2021-07-14 01:16:09'),
+(10, 'Nghia ne ae oi', 'Private', 'hahahahahah', '2021-07-15 01:14:32', '2021-07-15 01:14:32'),
+(11, 'Nghia day', 'Public', 'adasfgsgg', '2021-07-15 01:45:46', '2021-07-15 01:45:46');
 
 -- --------------------------------------------------------
 
@@ -97,14 +252,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(3, 'Lê Văn Nghĩa', 'meoem2712@gmail.com', NULL, '$2y$10$MDT4wh2v7ImDuStVGswdn.yHB/DrKXxEEHbqVXr16BfjzM5t6pv5W', NULL, '2021-07-13 20:24:03', '2021-07-13 20:24:03');
+(4, 'Quan123', '18211TT2471@mail.tdc.edu.vn', NULL, '$2y$10$lnwJ3NaaPD1IQfY2CmR2QuRMPFuuzxLlKPCTqUBayG0HnLYmu0j46', NULL, '2021-07-14 01:34:52', '2021-07-14 01:34:52'),
+(3, 'Lê Văn Nghĩa', 'meoem2712@gmail.com', NULL, '$2y$10$MDT4wh2v7ImDuStVGswdn.yHB/DrKXxEEHbqVXr16BfjzM5t6pv5W', NULL, '2021-07-13 20:24:03', '2021-07-13 20:24:03'),
+(12, 'Nghia', 'Nghia@gmail.com', NULL, '$2y$10$BMUwErOdCc/VtLinm8RU.ORc6wIkukBUBebqyP/hAw.HKznVOlmUa', NULL, '2021-07-14 21:01:16', '2021-07-14 21:01:16');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
